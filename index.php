6c4aa2e3085598ce1461748fcd96e2a0a252cf50
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-26 20:11:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 echo "<H1>Mini proyecto para el Taller de ESP32 UTEC</H1>";
 
 echo "En la siguiente URL mediante un GET podemos subir las coordenadas en formato lat long de nuestro GPS:<br>";
-echo "https://location-esp32.herokuapp.com/subir-lat-long.php?espid=Robert&lat=-34.2222&long=-54.111111<br>";
+echo "<b>https://location-esp32.herokuapp.com/subir-lat-long.php?espid=Robert&lat=-34.2222&long=-54.111111</b><br>";
 echo "Como hablamos, lo ideal seria usar un POST pero con el GET queda mas facil de usar y probar desde el navegador.<br>";
 echo "<br>";
 echo "Luego, mediante la un GET a la siguiente URL, podemos consultar la ultima posicion subida.<br>";
 echo "El codigo PHP hace una redireccion al sitio de google maps, para mostrar en el mapa la ubicacion.<br>";
-echo "https://location-esp32.herokuapp.com/ubicar.php?espid=Robert<br>";
+echo "<b>https://location-esp32.herokuapp.com/ubicar.php?espid=Robert</b><br>";
 echo "<br>";
 echo "Lo que se les pide, es mediante la ESP32 y su GPS conectado, extraer la lat y long del string que recibe y<br>";
 echo "armar una consulta GET para subir la ubicacion.<br>";
