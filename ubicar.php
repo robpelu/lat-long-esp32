fe0e25ece8e4c7679d64c56574f2556fc777e1fe
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-26 19:31:25 -0300

    Update ubicar.php

--- ubicar.php
+++ ubicar.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "ec2-52-202-185-87.compute-1.amazonaws.com";
-$database = "d79gdtjvldehc6";
-$username = "tsmjrcqunicnlx";
-$password = "156c2dadd79b76cf6782243d0c90c1af4f3003ae32a1d0c12157f815517db330";
+$servername = "ec2-54-197-34-207.compute-1.amazonaws.com";
+$database = "daer4tkav43vda";
+$username = "wcnixkgpgenwpj";
+$password = "cedbc8cac210d374fb88bcfb21b5dfc31c1034f0567a88fd569a4326fd1e0373";
 
-$id = $_GET["id"]; //igual a nombre en consulta "select"
+$espid = $_GET["espid"]; //igual a nombre en consulta "select"
 
 
 // Conectando y seleccionado la base de datos  
@@ -13,10 +13,12 @@ $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". 
     or die('Error - No se ha podido conectar: ' . pg_last_error());
 
 // Realizando una consulta SQL
-$query = "UPDATE espcommands SET espexecuted = 1 where id = '" . $id . "'" ;
-
+$query = "select * from location where espid = '" . $espid . "'" ;
 $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-echo "Marcado como ejecutado correctamente";
+while($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+         $myArray[] = $row;
+}
+echo json_encode($myArray);
 
 // Liberando el conjunto de resultados
 pg_free_result($result);
