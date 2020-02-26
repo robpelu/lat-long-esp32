2ee289d24987c72f58e7f97bcfe4dd3bad53058f
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-26 19:49:39 -0300

    Update ubicar.php

--- ubicar.php
+++ ubicar.php
@@ -17,8 +17,8 @@ $query = "select * from location where espid = '" . $espid . "'" ;
 $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 
 $row = pg_fetch_array($result, null, PGSQL_ASSOC);
-$lat = $row["lat"]
-$long = $row["long"]
+$lat = $row["lat"];
+$long = $row["long"];
 
 //echo json_encode($myArray);
 $redir = "Location: http://maps.google.com/maps?q=" . $lat . "," . $long;
