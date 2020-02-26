f3c1aba20887d4bdd1494c4a74346e7932598680
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-26 18:41:25 -0300

    Update getpos.php

--- getpos.php
+++ getpos.php
@@ -7,7 +7,7 @@ $database = "d79gdtjvldehc6";
 $username = "tsmjrcqunicnlx";
 $password = "156c2dadd79b76cf6782243d0c90c1af4f3003ae32a1d0c12157f815517db330";
 
-$nombre = $_GET['nombre'];
+espid = $_GET['espid'];
 
 
 // Conectando y seleccionado la base de datos  
@@ -15,7 +15,7 @@ $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". 
     or die('No se ha podido conectar: ' . pg_last_error());
 
 // Realizando una consulta SQL
-$query = "SELECT * FROM espcommands where espexecuted IS NULL AND espid like '" . $nombre . "' " ;
+$query = "SELECT * FROM location where espid like '" . $espid . "' " ;
 $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 
 while($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
